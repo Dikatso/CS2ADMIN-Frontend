689ff4eba2b8d5851822ef5ffdd1fcbd2c18848f
Dikatso <diikatsogaleboe@gmail.com>
2022-08-16 15:47:24 +0200

    feat: implement student view enqueries page

---
+++ src/pages/student/queries.tsx
@@ -0,0 +1,261 @@
+import { useRouter } from 'next/router';
+import {
+  createStyles,
+  Header,
+  Container,
+  Group,
+  Paper,
+  Transition,
+  useMantineTheme,
+} from '@mantine/core';
+import { IconPhoto, IconPrinter, IconCameraSelfie } from '@tabler/icons';
+import { Accordion } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { MantineLogo } from '@mantine/ds';
+import { Button } from '@chakra-ui/button';
+import { Box } from '@chakra-ui/react';
+import { TableSort } from '@/components/Student/TableSort';
+
+const HEADER_HEIGHT = 60;
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    position: `relative`,
+    zIndex: 1,
+  },
+
+  dropdown: {
+    position: `absolute`,
+    top: HEADER_HEIGHT,
+    left: 0,
+    right: 0,
+    zIndex: 0,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
+    borderTopWidth: 0,
+    overflow: `hidden`,
+
+    [theme.fn.largerThan(`sm`)]: {
+      display: `none`,
+    },
+  },
+
+  header: {
+    display: `flex`,
+    justifyContent: `space-between`,
+    alignItems: `center`,
+    height: `100%`,
+  },
+
+  links: {
+    [theme.fn.smallerThan(`sm`)]: {
+      display: `none`,
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan(`sm`)]: {
+      display: `none`,
+    },
+  },
+
+  link: {
+    display: `block`,
+    lineHeight: 1,
+    padding: `8px 12px`,
+    borderRadius: theme.radius.sm,
+    textDecoration: `none`,
+    color:
+      theme.colorScheme === `dark`
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === `dark`
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+    },
+
+    [theme.fn.smallerThan(`sm`)]: {
+      borderRadius: 0,
+      padding: theme.spacing.md,
+    },
+  },
+
+  linkActive: {
+    '&, &:hover': {
+      backgroundColor: theme.fn.variant({
+        variant: `light`,
+        color: theme.primaryColor,
+      }).background,
+      color: theme.fn.variant({ variant: `light`, color: theme.primaryColor })
+        .color,
+    },
+  },
+}));
+
+interface Links {
+  tag: string;
+  label: string;
+  variant: string;
+}
+
+const StudentMenu: React.FC = () => {
+  const router = useRouter();
+  const [opened] = useDisclosure(false);
+  const { classes } = useStyles();
+
+  const links: Array<Links> = [
+    { tag: `home`, label: `Home`, variant: `ghost` },
+    { tag: `signup`, label: `Enquiries`, variant: `ghost` },
+  ];
+  const items = links.map((link, index) => (
+    <Button
+      key={index}
+      colorScheme="teal"
+      size="md"
+      variant={link.variant}
+      onClick={() => {
+        if (link.tag === `home`) {
+          router.push(`/`);
+        } else {
+          router.push(`/signup`);
+        }
+      }}
+    >
+      {link.label}
+    </Button>
+  ));
+
+  const theme = useMantineTheme();
+  const getColor = (color: string) =>
+    theme.colors[color][theme.colorScheme === `dark` ? 5 : 7];
+
+  return (
+    <>
+      <Header height={HEADER_HEIGHT} mb={80} className={classes.root}>
+        <Container className={classes.header}>
+          <MantineLogo size={28} />
+          <Group spacing={5} className={classes.links}>
+            {items}
+          </Group>
+
+          <Transition
+            transition="pop-top-right"
+            duration={200}
+            mounted={opened}
+          >
+            {(styles) => (
+              <Paper className={classes.dropdown} withBorder style={styles}>
+                {items}
+              </Paper>
+            )}
+          </Transition>
+        </Container>
+      </Header>
+
+      <Box>
+        <Accordion variant="contained" mx={150}>
+          <Accordion.Item value="photos">
+            <Accordion.Control
+              icon={<IconPhoto size={20} color={getColor(`red`)} />}
+            >
+              Assignment Extensions
+            </Accordion.Control>
+            <Accordion.Panel>
+              <TableSort
+                tableType="assignment"
+                data={[
+                  {
+                    id: `MSHDIK008`,
+                    course: `CSC3002F`,
+                    assignNo: `1`,
+                    submitDate: `15-08-2022`,
+                    updateDate: `20-08-2022`,
+                    status: `Submitted`,
+                  },
+                  {
+                    id: `MTNKAR001`,
+                    course: `CSC3002F`,
+                    assignNo: `6`,
+                    submitDate: `20-08-2022`,
+                    updateDate: `20-08-2022`,
+                    status: `Updated`,
+                  },
+                ]}
+              />
+            </Accordion.Panel>
+          </Accordion.Item>
+
+          <Accordion.Item value="print">
+            <Accordion.Control
+              icon={<IconPrinter size={20} color={getColor(`blue`)} />}
+            >
+              Test Concessions
+            </Accordion.Control>
+            <Accordion.Panel>
+              <TableSort
+                tableType="test"
+                data={[
+                  {
+                    id: `MSHDIK008`,
+                    course: `CSC3002F`,
+                    assignNo: `1`,
+                    testNo: `2`,
+                    submitDate: `15-08-2022`,
+                    updateDate: `20-08-2022`,
+                    status: `Submitted`,
+                  },
+                  {
+                    id: `MTNKAR001`,
+                    course: `CSC3002F`,
+                    assignNo: ``,
+                    submitDate: `20-08-2022`,
+                    updateDate: `20-08-2022`,
+                    status: `Updated`,
+                  },
+                ]}
+              />
+            </Accordion.Panel>
+          </Accordion.Item>
+
+          <Accordion.Item value="camera">
+            <Accordion.Control
+              icon={<IconCameraSelfie size={20} color={getColor(`teal`)} />}
+            >
+              General Admin Queries
+            </Accordion.Control>
+            <Accordion.Panel>
+              <TableSort
+                tableType="assignment"
+                data={[
+                  {
+                    id: `MSHDIK008`,
+                    course: `CSC3002F`,
+                    assignNo: `1`,
+                    submitDate: `15-08-2022`,
+                    updateDate: `20-08-2022`,
+                    status: `Submitted`,
+                  },
+                  {
+                    id: `MTNKAR001`,
+                    course: `CSC3002F`,
+                    assignNo: ``,
+                    submitDate: `20-08-2022`,
+                    updateDate: `20-08-2022`,
+                    status: `Updated`,
+                  },
+                ]}
+              />
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+      </Box>
+    </>
+  );
+};
+
+export default StudentMenu;
