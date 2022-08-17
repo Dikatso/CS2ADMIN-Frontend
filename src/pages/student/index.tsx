3a56a80a3de192ea9e5514834e922856f99d870b
Dikatso <diikatsogaleboe@gmail.com>
2022-08-18 00:56:19 +0200

    feat: add student home page

--- src/pages/student/index.tsx
+++ src/pages/student/index.tsx
@@ -1,15 +1,82 @@
-import { Button, Box } from '@chakra-ui/react';
+import { StudentPageHeader } from '@/components/Student/Header';
+import { Footer } from '@/components/Shared/Footer';
+import { Box, Center, Text } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 
-const StudentHome = () => {
+const Convener: NextPage = () => {
   const router = useRouter();
+
   return (
-    <Box mx="25%">
-      <Button onClick={() => router.push(`/student/queries`)}>
-        View Submitted Enquiries
-      </Button>
+    <Box bg="white">
+      <StudentPageHeader />
+      <Box
+        height="100%"
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        mt={40}
+      >
+        <Box
+          bg="#F1F6F9"
+          width={250}
+          height={200}
+          mx="30px"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          boxShadow="base"
+          p="6"
+          rounded="2xl"
+          transition="transform 280ms"
+          _hover={{ cursor: `pointer`, transform: `translateY(-10px)` }}
+          onClick={() => router.push(`/student1`)}
+        >
+          <Center display="flex" flexDirection="column">
+            <Image
+              src={`/student/file.png`}
+              alt="Picture of the teacher illustration"
+              width={120}
+              height={120}
+            />
+            <Text fontSize="2xl" mt="5px">
+              Submit Enquiry
+            </Text>
+          </Center>
+        </Box>
+        <Box
+          bg="#F1F6F9"
+          width={250}
+          height={200}
+          mx="30px"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          boxShadow="base"
+          p="6"
+          rounded="2xl"
+          transition="transform 280ms"
+          _hover={{ cursor: `pointer`, transform: `translateY(-10px)` }}
+          onClick={() => router.push(`/student/queries`)}
+        >
+          <Center display="flex" flexDirection="column">
+            <Image
+              src={`/student/to-do-list.png`}
+              alt="Picture of the teacher illustration"
+              width={120}
+              height={120}
+            />
+            <Text fontSize="2xl" mt="5px">
+              View Enquiries
+            </Text>
+          </Center>
+        </Box>
+      </Box>
+      <Footer />
     </Box>
   );
 };
 
-export default StudentHome;
+export default Convener;
