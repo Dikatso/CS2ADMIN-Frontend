49f6a808be53e7bd02ba820ed02eae2b801f0d61
Botshxlo <Botshxlo.dev@gmail.com>
2022-09-06 02:38:30 +0200

    test: example for queries and mutations🧪

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,18 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { MantineProvider } from '@mantine/core';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
-    <ChakraProvider>
-      <MantineProvider>
-        <Component {...pageProps} />;
-      </MantineProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <MantineProvider>
+          <Component {...pageProps} />;
+        </MantineProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
