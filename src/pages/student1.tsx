31bf901d2f377fe57fd939bc8c29c3af19a6d312
Libhongo14 <92642329+Libhongo14@users.noreply.github.com>
2022-09-08 10:27:53 +0200

    feat: combo box working for displaying query options

--- src/pages/student1.tsx
+++ src/pages/student1.tsx
@@ -15,7 +15,8 @@ import { useState } from 'react';
 import { IconUpload } from '@tabler/icons';
 
 const StudentPage = () => {
-  const [value, setValue] = useState<string | null>(null);
+  const [option, setValue] = useState(``);
+  const handleChange = (event) => setValue(event.nativeEvent.target.value);
 
   return (
     <>
@@ -38,22 +39,55 @@ const StudentPage = () => {
           <Input type="string" />
           <FormLabel>Assignment Number</FormLabel>
           <Input type="string" width="full" />
-          {/* <FormHelperText>*This is a required field</FormHelperText> */}
-          <FormLabel>Extension Duration</FormLabel>
-          <Input type="string" />
+
           <FormLabel>Type</FormLabel>
 
-          <Select placeholder="Select option">
+          <Select defaultValue={option} onChange={handleChange}>
+            <option value="options">Select an option</option>
             <option value="extension">Assignment Extension</option>
             <option value="concession">Test concession</option>
             <option value="general">General Admin Query</option>
           </Select>
-          <FormLabel>Upload Medical Certificate</FormLabel>
-          <FileInput
-            mt={5}
-            placeholder="Medical certificate .pdf or .jpeg"
-            icon={<IconUpload size={14} />}
-          />
+          {option === `concession` ? (
+            <>
+              <FormLabel>Upload Medical Certificate</FormLabel>
+              <FileInput
+                mt={5}
+                placeholder="Medical certificate .pdf or .jpeg"
+              />
+
+              <FormLabel>Extension Duration</FormLabel>
+              <Input type="string" />
+              <FormLabel>Additional information:</FormLabel>
+              <Input type="string" />
+            </>
+          ) : (
+            <></>
+          )}
+          {option === `extension` ? (
+            <>
+              <FormLabel>Upload Medical Certificate</FormLabel>
+              <FileInput
+                mt={5}
+                placeholder="Medical certificate .pdf or .jpeg"
+                icon={<IconUpload size={14} />}
+              />
+              <FormLabel>Extension Duration</FormLabel>
+              <Input type="string" />
+              <FormLabel>Additional information:</FormLabel>
+              <Input type="string" />
+            </>
+          ) : (
+            <></>
+          )}
+          {option === `general` ? (
+            <>
+              <FormLabel>Query:</FormLabel>
+              <Input type="string" />
+            </>
+          ) : (
+            <></>
+          )}
           <Button mt={5} colorScheme="teal">
             Submit
           </Button>
