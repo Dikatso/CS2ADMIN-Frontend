e499c82facde2a814e8d417ce5f0c94368399212
Botshxlo <Botshxlo.dev@gmail.com>
2022-09-12 01:17:40 +0200

    fix: categorise enquiries by assessments 🚑

--- src/pages/convener/queries.tsx
+++ src/pages/convener/queries.tsx
@@ -1,17 +1,37 @@
 import { useRouter } from 'next/router';
 import { useMantineTheme } from '@mantine/core';
-import { IconPhoto, IconPrinter, IconCameraSelfie } from '@tabler/icons';
 import { Accordion } from '@mantine/core';
 import { Box } from '@chakra-ui/react';
 import { TableSort } from '@/components/Student/TableSort';
 import { ConvenerPageHeader } from '@/components/Convener/Header';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from '@/auth/Auth';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import { MdAssignment } from 'react-icons/md';
+import { HiPencil } from 'react-icons/hi';
+import { Skeleton } from '@mantine/core';
+import { Enquiry } from '@/types/convener';
+
+async function fetchEnquiries() {
+  const { data } = await axios.get<Promise<Enquiry[]>>(
+    `http://127.0.0.1:8000/apis/enquiries`,
+  );
+  return data;
+}
 
 const ConvenerMenu: React.FC = () => {
   const router = useRouter();
 
   const { isAuthenticated, getCurrentUser } = useAuth();
+  const [assignmentEnquiries, setAssignmentEnquiries] =
+    useState<Enquiry[]>(null);
+  const [testEnquiries, setTestEnquiries] = useState<Enquiry[]>(null);
+
+  const { data, isLoading, isSuccess } = useQuery(
+    `fetchEnquiries`,
+    fetchEnquiries,
+  );
 
   useEffect(() => {
     if (isAuthenticated()) {
@@ -28,6 +48,19 @@ const ConvenerMenu: React.FC = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (isSuccess && data != null) {
+      const assignmentEnquiries = data.filter(
+        (enquiry) => enquiry.type == `AssignmentExtension`,
+      );
+      setAssignmentEnquiries(assignmentEnquiries);
+      const testEnquiries = data.filter(
+        (enquiry) => enquiry.type == `TestConcession`,
+      );
+      setTestEnquiries(testEnquiries);
+    }
+  }, [isSuccess, data]);
+
   const theme = useMantineTheme();
   const getColor = (color: string) =>
     theme.colors[color][theme.colorScheme === `dark` ? 5 : 7];
@@ -36,100 +69,51 @@ const ConvenerMenu: React.FC = () => {
     <>
       <ConvenerPageHeader />
       <Box>
-        <Accordion variant="contained" mx={150} my={50}>
-          <Accordion.Item value="photos">
-            <Accordion.Control
-              icon={<IconPhoto size={20} color={getColor(`red`)} />}
-            >
-              Assignment Extensions
-            </Accordion.Control>
-            <Accordion.Panel>
-              <TableSort
-                tableType="assignment"
-                data={[
-                  {
-                    id: `56gt`,
-                    course: `CSC3002F`,
-                    assignNo: `1`,
-                    submitDate: `15-08-2022`,
-                    updateDate: `20-08-2022`,
-                    status: `Submitted`,
-                  },
-                  {
-                    id: `et456`,
-                    course: `CSC3002F`,
-                    assignNo: `6`,
-                    submitDate: `20-08-2022`,
-                    updateDate: `20-08-2022`,
-                    status: `Updated`,
-                  },
-                ]}
-              />
-            </Accordion.Panel>
-          </Accordion.Item>
+        <Accordion
+          variant="separated"
+          mx={150}
+          my={50}
+          transitionDuration={500}
+        >
+          {isLoading || assignmentEnquiries == null ? (
+            <Skeleton visible={true} height={60} />
+          ) : (
+            <Accordion.Item value="photos" mb={12}>
+              <Accordion.Control
+                icon={<MdAssignment size={20} color={getColor(`red`)} />}
+              >
+                Assignment Extensions
+              </Accordion.Control>
+              <Accordion.Panel>
+                <TableSort
+                  tableType="assignment"
+                  data={assignmentEnquiries}
+                  view="convener"
+                />
+              </Accordion.Panel>
+            </Accordion.Item>
+          )}
 
-          <Accordion.Item value="print">
-            <Accordion.Control
-              icon={<IconPrinter size={20} color={getColor(`blue`)} />}
-            >
-              Test Concessions
-            </Accordion.Control>
-            <Accordion.Panel>
-              <TableSort
-                tableType="test"
-                data={[
-                  {
-                    id: `43ww`,
-                    course: `CSC3002F`,
-                    assignNo: `1`,
-                    testNo: `2`,
-                    submitDate: `15-08-2022`,
-                    updateDate: `20-08-2022`,
-                    status: `Submitted`,
-                  },
-                  {
-                    id: `rr223`,
-                    course: `CSC3002F`,
-                    assignNo: ``,
-                    submitDate: `20-08-2022`,
-                    updateDate: `20-08-2022`,
-                    status: `Updated`,
-                  },
-                ]}
-              />
-            </Accordion.Panel>
-          </Accordion.Item>
-
-          <Accordion.Item value="camera">
-            <Accordion.Control
-              icon={<IconCameraSelfie size={20} color={getColor(`teal`)} />}
-            >
-              General Admin Queries
-            </Accordion.Control>
-            <Accordion.Panel>
-              <TableSort
-                tableType="assignment"
-                data={[
-                  {
-                    id: `wer23`,
-                    course: `CSC3002F`,
-                    assignNo: `1`,
-                    submitDate: `15-08-2022`,
-                    updateDate: `20-08-2022`,
-                    status: `Submitted`,
-                  },
-                  {
-                    id: `6r5ew`,
-                    course: `CSC3002F`,
-                    assignNo: ``,
-                    submitDate: `20-08-2022`,
-                    updateDate: `20-08-2022`,
-                    status: `Updated`,
-                  },
-                ]}
-              />
-            </Accordion.Panel>
-          </Accordion.Item>
+          {isLoading || testEnquiries == null ? (
+            <Skeleton visible={true} height={60} mt={15} />
+          ) : (
+            <Skeleton visible={isLoading}>
+              <Accordion.Item value="print" mb={12}>
+                <Accordion.Control
+                  icon={<HiPencil size={20} color={getColor(`blue`)} />}
+                >
+                  Test Concessions
+                </Accordion.Control>
+                <Accordion.Panel>
+                  <TableSort
+                    tableType="test"
+                    data={testEnquiries}
+                    view="convener"
+                  />
+                </Accordion.Panel>
+              </Accordion.Item>
+            </Skeleton>
+          )}
         </Accordion>
       </Box>
     </>
