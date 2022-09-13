ac1acb060ba4604b40ad3603df67d5388a02f305
Libhongo <mkxlib001@myuct.ac.za>
2022-09-13 12:37:02 +0200

    feat: query submission working but needs file attachment

--- src/pages/student1.tsx
+++ src/pages/student1.tsx
@@ -15,13 +15,51 @@ import { useState } from 'react';
 import { IconUpload } from '@tabler/icons';
 import { useRouter } from 'next/router';
 import { useAuth } from '@/auth/Auth';
+import { useMutation } from 'react-query';
+import axios from 'axios';
+
+interface createEnquiryDto {
+  userId: string;
+  courseCode: string;
+  extensionDuration: string;
+  assignmentNo: string;
+  type: string;
+  enquiryMessage: string;
+}
+
+export interface createEnquiryResponse {
+  enquiry: {
+    userId: string;
+    type: string;
+    title: string;
+    courseCode: string;
+    enquiryMessage: string;
+    extensionDuration: string;
+    attatchmentLink: string;
+    assignmentNo: string;
+    testNo: string;
+  };
+}
 
 const StudentPage = () => {
   const [option, setValue] = useState(``);
   const handleChange = (event) => setValue(event.nativeEvent.target.value);
   const router = useRouter();
-
   const { isAuthenticated, getCurrentUser } = useAuth();
+  const [coursecode, setCourseCode] = useState(``);
+  const [assignment, setAssignment] = useState(``);
+  const [type, setType] = useState(``);
+  const [duration, setDuration] = useState(``);
+  const [AdditionalInfo, setAdditionalInfo] = useState(``);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Course code`, coursecode);
+    console.log(`Assignment`, assignment);
+    console.log(`Type`, type);
+    console.log(`Duration`, duration);
+    console.log(handleChange);
+  };
 
   React.useEffect(() => {
     if (isAuthenticated()) {
@@ -37,6 +75,13 @@ const StudentPage = () => {
       router.push(`/`);
     }
   }, []);
+
+  const createEnquiryMutation = useMutation((enquiry: createEnquiryDto) => {
+    return axios.post<createEnquiryResponse>(
+      `http://127.0.0.1:8000/apis/enquiry/`,
+      enquiry,
+    );
+  });
 
   return (
     <>
@@ -54,57 +99,59 @@ const StudentPage = () => {
         rounded="md"
         bg="white"
       >
-        <FormControl>
+        <FormControl onSubmit={handleSubmit}>
           <FormLabel>Course Code</FormLabel>
-          <Input type="string" />
-          <FormLabel>Assignment Number</FormLabel>
-          <Input type="string" width="full" />
+          <Input
+            id="coursecode"
+            type="string"
+            onChange={(event) => setCourseCode(event.target.value)}
+            value={coursecode}
+          />
 
           <FormLabel>Type</FormLabel>
 
           <Select defaultValue={option} onChange={handleChange}>
             <option value="options">Select an option</option>
-            <option value="extension">Assignment Extension</option>
-            <option value="concession">Test concession</option>
+            <option value="AssignmentExtension">Assignment Extension</option>
+            <option value="TestConcession">Test concession</option>
             <option value="general">General Admin Query</option>
           </Select>
-          {option === `concession` ? (
+          {option === `TestConcession` || option === `AssignmentExtension` ? (
             <>
               <FormLabel>Upload Medical Certificate</FormLabel>
               <FileInput
                 mt={5}
                 placeholder="Medical certificate .pdf or .jpeg"
               />
-
+              {option === `AssignmentExtension` ? (
+                <>
+                  <FormLabel>Assignment Number</FormLabel>
+                  <Input
+                    type="string"
+                    onChange={(event) => setAssignment(event.target.value)}
+                    value={assignment}
+                  />
+                  <FormLabel>Extension duration</FormLabel>
+                  <Input
+                    type="string"
+                    onChange={(event) => setDuration(event.target.value)}
+                    value={duration}
+                  />
+                </>
+              ) : (
+                <></>
+              )}
               <Textarea
                 placeholder="Type here"
                 label="Additional information"
+                value={AdditionalInfo}
+                onChange={(event) => setAdditionalInfo(event.target.value)}
               />
             </>
           ) : (
             <></>
           )}
-          {option === `extension` ? (
-            <>
-              <FormLabel>Upload Medical Certificate</FormLabel>
-              <FileInput
-                mt={5}
-                placeholder="Medical certificate .pdf or .jpeg"
-                icon={<IconUpload size={14} />}
-              />
 
-              <DateRangePicker
-                label="Extension duration"
-                placeholder="Pick dates range"
-              />
-              <Textarea
-                placeholder="type here"
-                label="Additional information"
-              />
-            </>
-          ) : (
-            <></>
-          )}
           {option === `general` ? (
             <>
               <Textarea placeholder="type here" label="Query:" />
@@ -112,7 +159,24 @@ const StudentPage = () => {
           ) : (
             <></>
           )}
-          <Button mt={5} colorScheme="teal">
+          <Button
+            onClick={() => {
+              const {
+                user: { id },
+              } = getCurrentUser();
+              createEnquiryMutation.mutate({
+                userId: id,
+                type: option,
+                courseCode: coursecode,
+                enquiryMessage: AdditionalInfo,
+                extensionDuration: duration,
+                assignmentNo: assignment,
+              });
+            }}
+            type="submit"
+            mt={5}
+            colorScheme="teal"
+          >
             Submit
           </Button>
         </FormControl>
