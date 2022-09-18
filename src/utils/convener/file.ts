a5c461563d1320513e0a3a2d06a64ade77e4733e
NKNBOT001 <Botshxlo.dev@gmail.com>
2022-09-18 14:34:39 +0200

    chore: add file and tutor management utils💥

--- src/utils/convener/file.ts
+++ src/utils/convener/file.ts
@@ -1,5 +1,11 @@
-import { studentData } from '@/types/convener';
+import { studentData, studentTutorAllocation } from '@/types/convener';
 
+/**
+ * Converts a csv file into an array
+ * @param str
+ * @param delimiter
+ * @returns
+ */
 export const csvToArray = (str, delimiter = `,`) => {
   const headers = str.slice(0, str.indexOf(`\n`)).split(delimiter);
 
@@ -17,8 +23,15 @@ export const csvToArray = (str, delimiter = `,`) => {
   return arr;
 };
 
-export const extractDataIntoArray = async (
-  file: File[],
+/**
+ * Function to transform raw csv data into formatted student data,
+ * Checks to see if method is used to extract data in the student analysis or tutor management panel
+ * @param file - containing student data
+ * @returns generalised sorted array of student data
+ */
+export const extractStudentDataIntoArray = async (
+  file: File[] | File,
+  view: 'sa' | 'tm',
 ): Promise<studentData[]> => {
   const reader = new FileReader();
   const students: Array<studentData> = [];
@@ -51,11 +64,55 @@ export const extractDataIntoArray = async (
       i++;
     }
   };
-  reader.readAsText(file[0]);
+  reader.readAsText(view === 'sa' ? file[0] : file);
 
   return students;
 };
 
+/**
+ * Function to transform raw csv data into formatted student tutor allocation data
+ * @param file - containing student tutor assignment allocation
+ * @returns generalised sorted array of student tutor allocation
+ */
+export const extractStudentTutorAllocationDataIntoArray = async (
+  file: File,
+): Promise<studentTutorAllocation[]> => {
+  const reader = new FileReader();
+  const studentTutorAllocation: Array<studentTutorAllocation> = [];
+
+  reader.onabort = () => console.log(`file reading was aborted`);
+  reader.onerror = () => console.log(`file reading has failed`);
+  reader.onload = () => {
+    const content = reader.result;
+    const data = csvToArray(content);
+
+    let i = 0;
+    const len = data.length - 1;
+    while (i < len) {
+      const line = JSON.parse(JSON.stringify(data[i]));
+      const studentTutor: studentTutorAllocation = {
+        studentNumber: line.StudentNumber,
+        a1Tutor: line.A1_MARKER,
+        a2Tutor: line.A2_MARKER,
+        a3Tutor: line.A3_MARKER,
+        a4Tutor: line.A4_MARKER,
+        a5Tutor: line.A5_MARKER,
+        a6Tutor: line.A6_MARKER,
+      };
+      studentTutorAllocation.push(studentTutor);
+      i++;
+    }
+  };
+  reader.readAsText(file);
+
+  return studentTutorAllocation;
+};
+
+/**
+ * Transforms javascript objects into csv rows
+ * @param data
+ * @returns csv row string
+ */
 const objectToCsv = function (data) {
   const csvRows = [];
   const headers = Object.keys(data[0]);
@@ -74,6 +131,12 @@ const objectToCsv = function (data) {
   return csvRows.join(`\n`);
 };
 
+/**
+ * Downloads student data into a csv file
+ * @param studentData
+ * @param assessmentType - assessment name .i.e Assignment 1, Test 2
+ * @param ranking - chart category .i.e Outstanding, Critical, Moderate...
+ */
 export const exportToCsv = (
   studentData: studentData[],
   assessmentType: string,
